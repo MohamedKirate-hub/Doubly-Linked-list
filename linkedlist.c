920b182a3c56706f2a32fa0a51996dc43b362363
Mohamed Kirate <mkirate@e1r4p7.1337.ma>
2025-12-05 17:14:01 +0100

    v3

--- linkedlist.c
+++ linkedlist.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node {
+typedef struct Node 
+{
     int data;
     struct Node *next;
     struct Node *prev;
 } Node;
 
-Node *ft_CreateNode(int data)
+Node *ft_create_node(int data)
 {
     Node *newNode = malloc(sizeof(Node));
     newNode -> data = data;
@@ -15,27 +16,25 @@ Node *ft_CreateNode(int data)
     newNode -> prev = NULL;
     return (newNode);
 }
-
-void ft_add_front(Node **head, int data)
+void    ft_add_front(Node **head, int data)
 {
-    Node *newNode = ft_CreateNode(data);
-    if (!*head)
+    Node *newNode = ft_create_node(data);
+    if (*head == NULL)
     {
         (*head) = newNode;
         return;
     }
-    (*head) -> prev = newNode;
     newNode -> next = (*head);
+    (*head) -> prev = newNode;
     (*head) = newNode;
 }
-
-void ft_add_back(Node **head, int data)
+void    ft_add_back(Node **head, int data)
 {
-    Node *newNode = ft_CreateNode(data);
+    Node *newNode = ft_create_node(data);
     Node *temp = NULL;
-    if (!*head)
+    if (*head == NULL)
     {
-        *head = newNode;
+        (*head) = newNode;
         return;
     }
     temp = *head;
@@ -47,87 +46,70 @@ void ft_add_back(Node **head, int data)
 
 void ft_print_list(Node *head)
 {
-    Node *temp = head;
     Node *tail = NULL;
-    printf("\tthe next steps: \n");
-    while (temp != NULL)
+    if (head == NULL)
+        return;
+    printf("\nnext data\n");
+    while (head != NULL)
     {
-        printf("\t%d\n", temp -> data);
-        if (temp -> next == NULL)
-            tail = temp;
-        temp = temp -> next;
+        printf("%d\n", head -> data);
+        if (head -> next == NULL)
+            tail = head;
+        head = head -> next;
     }
-    // printf("\tthe prev steps: \n");
+    // printf("\nprev data\n");
     // while (tail != NULL)
     // {
-    //     printf("\t%d\n", tail -> data);
+    //     printf("%d\n", tail -> data);
     //     tail = tail -> prev;
     // }
 }
-
-void ft_swap_list(Node **head)
+void    ft_swap_two(Node **head)
 {
     int temp = 0;
-    if (*head == NULL || (*head) -> next == NULL)
+    if (head == NULL || *head == NULL || (*head) -> next == NULL)
         return;
     temp = (*head) -> data;
     (*head) -> data = (*head) -> next -> data;
     (*head) -> next -> data = temp;
 }
-void ft_rotate_list(Node **head)
-{
-    Node *struct_temp = NULL;
-    int int_temp = 0;
 
-    struct_temp = (*head);
-    if (struct_temp == NULL || struct_temp ->next == NULL)
-        return ;
-    while (struct_temp -> next != NULL)
-    {
-        int_temp = struct_temp -> data;
-        struct_temp -> data = struct_temp -> next -> data;
-        struct_temp -> next -> data = int_temp;
-        struct_temp = struct_temp -> next;
-    }
-}
-
-int ft_list_size(Node *head)
+void ft_delete_front(Node **head)
 {
-    int i = 0;
-    while (head != NULL)
-    {
-        i++;
-        head = head -> next;
-    }
-    return (i);
-}
-
-void ft_reverse_list(Node **head)
-{
-    Node *cur = *head;
     Node *temp = NULL;
-    if (*head == NULL || head == NULL)
-        return ;
-    while (cur != NULL)
-    {
-        temp = cur -> next;
-        cur -> next = cur -> prev;
-        cur -> prev = temp;
-        if (cur -> prev == NULL)
-            *head = cur;
-        cur = cur -> prev;
-    }
+    if (head == NULL || *head == NULL)
+        return;
+    temp = (*head);
+    (*head) = (*head) -> next;
+    if (*head != NULL)
+        (*head) -> prev = NULL;
+    free(temp);
+}
+
+void ft_push_a(Node **a, Node **b)
+{
+    ft_add_front(a, (*b) -> data);
+    ft_delete_front(b);
 }
 
 int main()
 {
     Node *head = NULL;
-    ft_add_front(&head, 50);
     ft_add_front(&head, 40);
     ft_add_front(&head, 30);
     ft_add_front(&head, 20);
     ft_add_front(&head, 10);
-    //ft_swap_list(&head);
-    //ft_reverse_list(&head);
     ft_print_list(head);
+
+    Node *head2 = NULL;
+    ft_add_front(&head2, 41);
+    ft_add_front(&head2, 32);
+    ft_add_front(&head2, 23);
+    ft_add_front(&head2, 14);
+
+    ft_print_list(head2);
+
+    push_a(&head, &head2);
+    ft_print_list(head);
+    ft_print_list(head2);
 }
